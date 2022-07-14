394ed0f8dbb13e9e82b94f86fabc7c2f0e95b0f8
Justin Moon <mail@justinmoon.com>
2022-07-14 20:01:41 +0200

    Save LightningGateway to federation db

--- minimint-core/src/gateway.rs
+++ minimint-core/src/gateway.rs
@@ -1,7 +1,8 @@
 use bitcoin::secp256k1;
+use minimint_api::encoding::{Decodable, Encodable};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, Encodable, Decodable)]
 pub struct LightningGateway {
     pub mint_pub_key: secp256k1::XOnlyPublicKey,
     pub node_pub_key: secp256k1::PublicKey,
--- minimint/src/db.rs
+++ minimint/src/db.rs
@@ -3,11 +3,14 @@ use crate::transaction::Transaction;
 use minimint_api::db::DatabaseKeyPrefixConst;
 use minimint_api::encoding::{Decodable, Encodable};
 use minimint_api::{PeerId, TransactionId};
+use bitcoin::secp256k1::PublicKey;
+use minimint_core::gateway::LightningGateway;
 use std::fmt::Debug;
 
 pub const DB_PREFIX_PROPOSED_TRANSACTION: u8 = 0x01;
 pub const DB_PREFIX_ACCEPTED_TRANSACTION: u8 = 0x02;
 pub const DB_PREFIX_DROP_PEER: u8 = 0x03;
+pub const DB_PREFIX_LIGHTNING_GATEWAY: u8 = 0x04;
 
 #[derive(Debug, Encodable, Decodable)]
 pub struct ProposedTransactionKey(pub TransactionId);
@@ -52,4 +55,22 @@ impl DatabaseKeyPrefixConst for DropPeerKeyPrefix {
     const DB_PREFIX: u8 = DB_PREFIX_DROP_PEER;
     type Key = DropPeerKey;
     type Value = ();
+}
+
+#[derive(Debug, Encodable, Decodable)]
+pub struct LightningGatewayKey(pub PublicKey);
+
+impl DatabaseKeyPrefixConst for LightningGatewayKey {
+    const DB_PREFIX: u8 = DB_PREFIX_LIGHTNING_GATEWAY;
+    type Key = Self;
+    type Value = LightningGateway;
+}
+
+#[derive(Debug, Encodable, Decodable)]
+pub struct LightningGatewayKeyPrefix;
+
+impl DatabaseKeyPrefixConst for LightningGatewayKeyPrefix {
+    const DB_PREFIX: u8 = DB_PREFIX_LIGHTNING_GATEWAY;
+    type Key = LightningGatewayKey;
+    type Value = LightningGateway;
 }
