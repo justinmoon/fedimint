b7926c85f88fced4c44bdb1898112096eb895450
Justin Moon <mail@justinmoon.com>
2023-03-12 16:25:01 -0500

    feat: fixtures spawns fedimintd

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -230,7 +230,6 @@ function start_esplora() {
 
 function start_federation() {
   echo "starting federation"
-  await_bitcoin_rpc
 
   START_SERVER=${1:-0}
   END_SERVER=${2:-$FM_FED_SIZE}
@@ -238,8 +237,8 @@ function start_federation() {
   # Start the federation members inside the temporary directory
   for ((ID=START_SERVER; ID<END_SERVER; ID++)); do
     echo "starting mint $ID"
-    export FM_PASSWORD="pass$ID"
-    ( ($FM_BIN_DIR/fedimintd $FM_CFG_DIR/server-$ID 2>&1 & echo $! >&3 ) 3>>$FM_PID_FILE | sed -e "s/^/mint $ID: /" ) &
+    $FM_BIN_DIR/fixtures fedimintd $ID &
+    echo $! >> $FM_PID_FILE
   done
   echo "started federation"
 }
