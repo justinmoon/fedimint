7f085830a2c15e34adb32045fc7db11cb3ef027a
Justin Moon <mail@justinmoon.com>
2023-04-26 11:52:02 -0500

    hack: try to invoke tmuxinator

---
+++ scripts/hack.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Runs a CLI-based integration test
+
+set -euo pipefail
+export RUST_LOG="${RUST_LOG:-info}"
+source ./scripts/build.sh
+
+$FM_BIN_DIR/fedimint-bin-tests tmuxinator
