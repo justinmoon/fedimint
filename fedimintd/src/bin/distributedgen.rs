1da005478a54ab5948ba97cd5a0cf10163e3eb4d
Justin Moon <mail@justinmoon.com>
2023-05-15 22:15:24 -0500

    thrashing: attempt to run dkg with dummy module

--- fedimintd/src/bin/distributedgen.rs
+++ fedimintd/src/bin/distributedgen.rs
@@ -1,6 +1,11 @@
+use fedimint_dummy_server::DummyGen;
 use fedimintd::distributed_gen::DistributedGen;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    DistributedGen::new()?.with_default_modules().run().await
+    DistributedGen::new()?
+        .with_default_modules()
+        .with_module(DummyGen)
+        .run()
+        .await
 }
