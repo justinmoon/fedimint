8b5675e126f567936eab6231aee01c6cb5c8915e
Justin Moon <mail@justinmoon.com>
2023-05-15 22:48:13 -0500

    fix: run dkg with dummy module

--- fedimintd/src/bin/distributedgen.rs
+++ fedimintd/src/bin/distributedgen.rs
@@ -1,3 +1,5 @@
+use fedimint_core::Amount;
+use fedimint_dummy_common::config::{DummyGenParams, DummyGenParamsConsensus};
 use fedimint_dummy_server::DummyGen;
 use fedimintd::distributed_gen::DistributedGen;
 
@@ -6,6 +8,16 @@ async fn main() -> anyhow::Result<()> {
     DistributedGen::new()?
         .with_default_modules()
         .with_module(DummyGen)
+        .with_extra_module_gens_params(
+            3,
+            fedimint_dummy_common::KIND,
+            DummyGenParams {
+                local: Default::default(),
+                consensus: DummyGenParamsConsensus {
+                    tx_fee: Amount::ZERO,
+                },
+            },
+        )
         .run()
         .await
 }
